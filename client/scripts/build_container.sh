6f71c9d83a97e72b82c8931240391e05c53856d9
lidia <lidia.zhabo99@gmail.com>
2021-04-06 12:56:51 +0300

    updated readme

--- client/scripts/build_container.sh
+++ client/scripts/build_container.sh
@@ -7,12 +7,5 @@ else
   VERSION="${1}"
 fi
 set -e
-# Закомментил процес копирования и билда common модуля. Вооюще нужен более красивый способ подключать локальные пакеты к проекту
-#mkdir calculator-common
-#cp -r ../calculator-common/src ./calculator-common/src
-#cp  ../calculator-common/package.json ./calculator-common/package.json
-#cp  ../calculator-common/package-lock.json ./calculator-common/package-lock.json
-#cp  ../calculator-common/tsconfig.json ./calculator-common/tsconfig.json
 
-docker build -t registry.abetalife.com/react-spa-project:${VERSION} .
-#rm -r calculator-common
+docker build -t <url> .
